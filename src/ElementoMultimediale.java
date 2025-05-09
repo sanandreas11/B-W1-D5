4f4aa28b6e8fa33d74beb3a05ca5cfb216c119f0
sanandreas11 <andreaandonis@outlook.com>
2025-05-09 10:10:09 +0200

    adds basic classes

---
+++ src/ElementoMultimediale.java
@@ -0,0 +1,9 @@
+public abstract class ElementoMultimediale {
+    protected String titolo;
+
+    public ElementoMultimediale(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public abstract void esegui();
+}
